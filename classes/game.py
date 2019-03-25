0d90abfc366415210b56d8da162a9513f4cf770f
thomashenson <thomas.henson@qa.com>
2019-03-25 14:30:10 +0000

    Added items functionality and some formatting changes.

--- classes/game.py
+++ classes/game.py
@@ -1,5 +1,4 @@
 import random
-from .magic import Spell
 
 class bcolors:
     HEADER = '\033[95m'
@@ -13,7 +12,7 @@ class bcolors:
 
 
 class Person:
-    def __init__(self, hp, mp, atk, df, magic):
+    def __init__(self, hp, mp, atk, df, magic, items):
         self.maxhp = hp
         self.hp = hp
         self.maxmp = mp
@@ -22,7 +21,8 @@ class Person:
         self.atkh = atk + 10
         self.df = df
         self.magic = magic
-        self.actions = ["Attack", "Magic"]
+        self.items = items
+        self.actions = ["Attack", "Magic", "Items"]
 
     def generate_damage(self):
         return random.randrange(self.atkl, self.atkh)
@@ -55,15 +55,21 @@ class Person:
 
     def choose_action(self):
         i = 1
-        print(bcolors.OKBLUE + bcolors.BOLD + "Actions" + bcolors.ENDC)
+        print("\n" + bcolors.OKBLUE + bcolors.BOLD + "ACTIONS:" + bcolors.ENDC)
         for item in self.actions:
-            print(str(i) + ":", item)
+            print("    " + str(i) + ":", item)
             i += 1
 
     def choose_magic(self):
         i = 1
-        print(bcolors.OKBLUE + bcolors.BOLD + "Magic" + bcolors.ENDC)
+        print("\n" + bcolors.OKBLUE + bcolors.BOLD + "MAGIC:" + bcolors.ENDC)
         for spell in self.magic:
-            print(str(i) + ":", spell.name, "{cost:", str(spell.cost) + "}")
+            print("    " + str(i) + ":", spell.name, "{cost:", str(spell.cost) + "}")
             i += 1
 
+    def choose_item(self):
+        i = 1
+        print("\n" + bcolors.OKGREEN + bcolors.BOLD + "ITEMS:" + bcolors.ENDC)
+        for item in self.items:
+            print("    " + str(i) + ".", item.name, ":", item.desc, " (x5)")
+            i += 1
---
+++ classes/inventory.py
@@ -0,0 +1,6 @@
+class Item:
+    def __init__(self, name, type, desc, prop):
+        self.name = name
+        self.type = type
+        self.desc = desc
+        self.prop = prop
